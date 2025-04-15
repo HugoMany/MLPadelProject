a4eb627a1089ea407082730128c6fa860962e2b0
HugoMany <90179566+HugoMany@users.noreply.github.com>
2025-04-15 09:46:46 +0200

    Delete Exemple in nb, and comment/doc in functions folder

--- Functions/energy_per_frequency_band_from_spectrum.py
+++ Functions/energy_per_frequency_band_from_spectrum.py
@@ -1,6 +1,28 @@
 import numpy as np
 
 def energy_per_frequency_band_from_spectrum(spectrum, freqs, band_width):
+    """
+    Calculate the energy per frequency band from a given spectrum.
+    This function divides the frequency range into bands of a specified width
+    and computes the energy (sum of squared magnitudes) for each band. It also
+    calculates the center frequency of each band.
+    Parameters:
+        spectrum (numpy.ndarray): The spectrum values (e.g., magnitudes or amplitudes).
+        freqs (numpy.ndarray): The corresponding frequencies of the spectrum.
+        band_width (int): The width of each frequency band.
+    Returns:
+        tuple:
+            - band_energies (list): A list of energy values for each frequency band.
+            - band_frequencies (list): A list of center frequencies for each band.
+
+    Example :
+
+    >>> band_energies, band_frequencies = energy_per_frequency_band_from_spectrum(spectrum, freqs, bd)
+
+    >>> band_energies => the energy for each band
+    >>> band_frequencies => frequency for each band
+
+    """
     # Calculate energy per band
     band_energies = []
     band_frequencies = []
